94486f8dba2bb4247c87fb3b542e078f39468339
Benson Yu <bennywhyu@gmail.com>
2018-11-13 22:09:33 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(string)
   new_array=[]
   new_array=string.split(" ")
   new_array.each{|x|
-    x.split("").reverse
+    y=x.split("")
+    y.reverse
   }
   return new_array
 end
